41ab80daf93e7c44bd172ba02d6a088f1db3f00f
Henri Lehtonen <henri.lehtonen.87@gmai.com>
2026-03-10 21:05:03 +0100

    feat: set up app routing with upload and players routes

---
+++ fm-stats-angular/src/app/components/player-table/player-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-table',
+  standalone: true,
+  template: '<p>Players table placeholder</p>',
+})
+export class PlayerTableComponent {}
---
+++ fm-stats-angular/src/app/components/upload/upload.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload',
+  standalone: true,
+  template: '<p>Upload page placeholder</p>',
+})
+export class UploadComponent {}
